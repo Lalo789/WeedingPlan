f69c5b9f87f385b3c8248bedcf7fe6bd2a0286e1
Lalo789 <54688776+Lalo789@users.noreply.github.com>
2025-10-08 12:13:06 -0600

    Delete WeddingPlan directory
    Archivo duplicado del Proyecto

--- WeddingPlan/config.py
+++
@@ -1,3 +0,0 @@
-#Conexion a la base de datos directo con postgres
-SQLALCHEMY_DATABASE_URI = "postgresql://postgres:12345@localhost:5432/WeddingPlan?client_encoding=latin1"
-SQLALCHEMY_TRACK_MODIFICATIONS = False
